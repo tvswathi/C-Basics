f87ad5f39f41523b9c86f67ba6e76490c16c8cc7
tvswathi <43932903+tvswathi@users.noreply.github.com>
2020-12-24 13:46:00 -0800

    Create ffp.cpp

---
+++ function/ffp.cpp
@@ -0,0 +1,13 @@
+// create a function which get the firstname and pass fullname
+
+#include <iostream>
+using namespace std;
+void fun(string gnames){
+    cout<<gnames<<"unni \n";
+}
+int main(){
+    fun("swathi");
+    fun("ram ");
+    
+    return 0;
+}
