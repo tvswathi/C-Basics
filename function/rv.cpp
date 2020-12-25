d80a1b7b450878104d88528a43607670286c8cb7
tvswathi <43932903+tvswathi@users.noreply.github.com>
2020-12-25 00:57:10 -0800

    Create rv.cpp

---
+++ function/rv.cpp
@@ -0,0 +1,12 @@
+// Return value in a function
+
+#include<iostream>
+using namespace std;
+
+int func(int a,int b){
+    return (a+b);
+}
+int main(){
+    cout<<"Sum:"<<func(3,4);
+    return 0;
+}
